c71411885bae73591040fe7e5be233e3edb04990
danembb <dbrophy720@gmail.com>
2021-05-20 12:53:56 -0600

    Create welcome message

--- lib/message.rb
+++ lib/message.rb
@@ -2,4 +2,9 @@ class Message
 
   def initialize
   end
+
+  def welcome
+    #why doesnt this work with puts, p or print? (also whats the difference)
+    'Welcome to MASTERMIND'
+  end
 end
--- spec/message_spec.rb
+++ spec/message_spec.rb
@@ -8,4 +8,12 @@ RSpec.describe Message do
       expect(message).to be_an_instance_of(Message)
     end
   end
+
+  describe '#methods' do
+    it '#can display a welcome message' do
+      message = Message.new
+
+      expect(message.welcome).to eq('Welcome to MASTERMIND')
+    end
+  end
 end

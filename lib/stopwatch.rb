e20b7fd84b0698503e5ff4f6de6e14325f8f3a32
danembb <dbrophy720@gmail.com>
2021-05-22 19:06:24 -0600

    Create lapsed time methods for stopwatch class

--- lib/stopwatch.rb
+++ lib/stopwatch.rb
@@ -1,11 +1,19 @@
 class Stopwatch
 
-  def initialize()
+  def initialize
     @start = Time.now
   end
 
-  # def elapsed_time
-  #   now = Time.now
-  #   elapsed = now - @start
-  # end
+  def elapsed_time
+    now = Time.now
+    elapsed = (now - @start).round(2)
+  end
+
+  def elapsed_minutes
+    (elapsed_time/60).round(0)
+  end
+
+  def elapsed_seconds
+    (elapsed_time % 60).round(0)
+  end
 end

0a733dff3acc0fd5cd746d4365058f105bbcebc6
Brett Jones <bhjones4545@gmail.com>
2021-05-26 17:57:48 -0600

    Final refactor

--- lib/message.rb
+++ lib/message.rb
@@ -5,13 +5,16 @@ class Message
 
   def welcome
     'Welcome to MASTERMIND
-    
-    Would you like to (p)lay, read the (i)nstructions, or (q)uit?'
+    '
+  end
+
+  def welcome_query
+    'Would you like to (p)lay, read the (i)nstructions, or (q)uit?'
   end
 
   def instructions
-    "Goal: Correctly guess the codemaker's code.
-    The codemaker will generate a 4-character code sequence.
+    "Goal: Correctly guess my code.
+    I will generate a 4-character code sequence.
     It is up to you to guess the correct sequence by inputting the correct colors (red, green, blue and yellow) in the correct order.
     These colors are implemented in-game with the characters in the following chart (case-insensitive):
     Red = 'r'
--- spec/message_spec.rb
+++ spec/message_spec.rb
@@ -15,14 +15,20 @@ RSpec.describe Message do
   describe 'methods' do
     it 'can display a welcome message' do
       message = Message.new
-      expected = "Welcome to MASTERMIND\n    \n    Would you like to (p)lay, read the (i)nstructions, or (q)uit?"
-      expect(message.welcome).to eq(expected)
+
+      expect(message.welcome).to eq("Welcome to MASTERMIND\n    ")
+    end
+
+    it 'can display a welcome query' do
+      message = Message.new
+
+      expect(message.welcome_query).to eq('Would you like to (p)lay, read the (i)nstructions, or (q)uit?')
     end
 
     it 'can display instructions' do
       message = Message.new
 
-      expected = "Goal: Correctly guess the codemaker's code.\n    The codemaker will generate a 4-character code sequence.\n    It is up to you to guess the correct sequence by inputting the correct colors (red, green, blue and yellow) in the correct order.\n    These colors are implemented in-game with the characters in the following chart (case-insensitive):\n    Red = 'r'\n    Green = 'g'\n    Blue = 'b'\n    Yellow = 'y'\n    You may be able to (c)heat while playing, too. But what fun is that?\n    Enter (p)lay to play the game or (q)uit to quit!"
+      expected = "Goal: Correctly guess my code.\n    I will generate a 4-character code sequence.\n    It is up to you to guess the correct sequence by inputting the correct colors (red, green, blue and yellow) in the correct order.\n    These colors are implemented in-game with the characters in the following chart (case-insensitive):\n    Red = 'r'\n    Green = 'g'\n    Blue = 'b'\n    Yellow = 'y'\n    You may be able to (c)heat while playing, too. But what fun is that?\n    Enter (p)lay to play the game or (q)uit to quit!"
 
       expect(message.instructions).to eq(expected)
     end
